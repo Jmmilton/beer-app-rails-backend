bcbabdf4db668c9d4b9ac5c222def557ac5c97e7
jay <jason@mobilefringe.com>
2026-03-06 23:19:07 -0500

    set root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   get '/csrf_token', to: 'application#csrf_token'
-
-
   post '/save-beer/:variant', to: 'beers#create'
+
+  root to: 'beers#index'
   
   resources :beers do
     collection do
@@ -14,5 +14,4 @@ Rails.application.routes.draw do
     end
   end
 
-  
 end

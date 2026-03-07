168cc11fd267c6b83529b5559287e63a79a3106c
jay <jason@mobilefringe.com>
2026-03-07 18:05:09 -0500

    cors fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,9 @@ require 'rack/cors'
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://localhost:5173',
-            'https://jmmilton.github.io'
+            'https://jmmilton.github.io',
+            'https://mybrews.app',
+            'http://mybrews.app'
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
   end
 end

e49b4f18139200a7f40dbd5c5bd80cd1db1209db
jay <jason@mobilefringe.com>
2026-03-09 22:55:34 -0400

    fix unconfirmed email migration error

--- db/migrate/20260309025602_add_unconfirmed_email_to_users.rb
+++ db/migrate/20260309025602_add_unconfirmed_email_to_users.rb
@@ -1,5 +1,5 @@
 class AddUnconfirmedEmailToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :unconfirmed_email, :string
+    add_column :users, :unconfirmed_email, :string unless column_exists?(:users, :unconfirmed_email)
   end
 end

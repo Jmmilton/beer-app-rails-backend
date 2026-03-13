434382361f2f97009f26f420c416a5f8af94781c
jay <jason@mobilefringe.com>
2026-03-12 20:13:38 -0400

    beer model ref

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -15,8 +15,10 @@ class Beer < ApplicationRecord
     return nil unless image.attached?
     blob = image.blob
     "https://#{ENV['SUPABASE_PROJECT_REF']}.supabase.co/storage/v1/object/public/#{ENV['SUPABASE_BUCKET']}/#{blob.key}"
+  rescue StandardError => e
+    Rails.logger.error "Image URL failed: #{e.message}"
+    nil
   end
-
 end
 
  

6346d4cd9bb6a1390fef2663cbb6932a9caecfb9
jay <jason@mobilefringe.com>
2026-03-22 16:59:35 -0400

    custom email page

--- app/mailers/custom_devise_mailer.rb
+++ app/mailers/custom_devise_mailer.rb
@@ -11,4 +11,17 @@ class CustomDeviseMailer < Devise::Mailer
       template_name: 'reset_password_instructions'
     )
   end
+
+  def confirmation_instructions(record, token, opts = {})
+    @token = token
+    @resource = record
+    @url = "#{ENV['FRONTEND_URL']}/#/confirm-email?confirmation_token=#{token}"
+    mail(
+      to: record.email,
+      from: Devise.mailer_sender,
+      subject: "Confirm your MyBrews account",
+      template_path: 'devise/mailer',
+      template_name: 'confirmation_instructions'
+    )
+  end
 end
